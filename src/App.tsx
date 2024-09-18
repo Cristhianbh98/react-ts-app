c56cb45c1aa82596912f9251cc3d4e15231e5c84
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2024-09-17 20:17:45 -0500

    practice 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
-import RandomFox from "./components/RandomFox"
+import { useState } from "react";
+import type { MouseEventHandler } from "react";
+import LazyImage from "./components/RandomFox"
+
+type ImageItem = {
+  url: string
+  id: string
+}
 
 const getRandomNumber = (): number => {
   return Math.floor(Math.random() * 123) + 1;
 }
 
+const generateUniqueId = (): string => {
+  return Math.random().toString(36).slice(2, 9);
+}
 
 function App() {
+  const link = 'https://randomfox.ca/images/'
+  const [images, setImages] = useState<ImageItem[]>([])
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = () => {
+    const newItem: ImageItem = {
+      url: `${link}${getRandomNumber()}.jpg`,
+      id: generateUniqueId()
+    }
+
+    setImages([...images, newItem])
+  }
+
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <RandomFox img={`https://randomfox.ca/images/${getRandomNumber()}.jpg`} alt='Random'/>
+    <main className="flex justify-center pt-4">
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello Platzi!
+        </h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>
+          Agregar Zorro
+        </button>
+        {
+          images.map(item => (
+            <div key={item.id} className="p-4">
+              <LazyImage 
+                src={item.url} 
+                width={300} 
+                height='auto'
+                onLazyLoad={() => console.log('Lazy load')}
+                />
+            </div>
+          ))
+        }
+      </div>
     </main>
   )
 }
--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,14 +1,36 @@
-type Props = {
-  img: string
-  alt?: string
+import { useRef, useEffect, useState } from "react"
+import type { ImgHTMLAttributes } from "react"
+
+interface Props extends ImgHTMLAttributes<HTMLImageElement> {
+  src: string
+  onLazyLoad?: () => void
 }
 
-const RandomFox = ({img, alt = ''}: Props): JSX.Element => {
+const LazyImage = ({src, onLazyLoad, ...props}: Props): JSX.Element => {
+  const [image, setImage] = useState<string>('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=')
+  const node = useRef<HTMLImageElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          console.log('Visible')
+          if (typeof onLazyLoad === 'function') onLazyLoad()
+          setImage(src)
+        }
+      })
+    })
+    
+    if (node.current) observer.observe(node.current)
+
+    return () => observer.disconnect()
+  }, [src])
+
   return (
     <div>
-      <img src={img} width={320} height='auto' className="rounded" alt={alt}/>
+      <img {...props} ref={node} src={image}/>
     </div>
   )
 }
 
-export default RandomFox
+export default LazyImage
